f11cdb54240db2dc5b0fb7ada81eeaa5a076ae8b
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-06 17:57:26 -0300

    Feat: create test signUpTest

--- src/test/java/TestAPI.java
+++ src/test/java/TestAPI.java
@@ -1,5 +1,23 @@
+import io.restassured.http.ContentType;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.*;
+
 public class TestAPI {
+
+    @Test
+    public void signUpTest() {
+        String url = "https://api.thecatapi.com/v1/user/passwordlesssignup";
+        String body = "{\"email\": \"amonteiro@email.com\", \"appDescription\": \"Testing The Cat API with REST Assured and JUnit\"}";
+
+        given()
+                .contentType(ContentType.JSON).accept(ContentType.JSON)
+                .body(body)
+                .when()
+                .post(url)
+                .then()
+                .statusCode(200)
+        ;
+    }
 
 }
